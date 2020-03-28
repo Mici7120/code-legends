99cf9c2822680461740bf5724d9df4d1f60d3cd6
winandtu <jefersson.arevalo@correounivalle.edu.co>
2020-03-28 22:17:18 +0000

    Cambios en ejercito, box y tablero

--- avatar.cpp
+++ avatar.cpp
@@ -1,27 +1,6 @@
 #include "avatar.h"
 
-/*
-Avatar::Avatar(){
 
-}
-
-void Avatar::setTipo(string tipo){
-  Tipo = tipo;
-  if(tipo=="luchador"){
-    Poder = .7;
-    Mana = .6;
-    Vida = 4;
-  }else if(tipo=="tirador"){
-    Poder = 1;
-    Mana = .3;
-    Vida = 3;
-  }else if(tipo=="mago"){
-    Poder = .2;
-    Mana = 1;
-    Vida = 5;
-  }
-
-} */
 
 // Para modificar la vida 
 
