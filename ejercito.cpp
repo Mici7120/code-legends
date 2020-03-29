d75e08265347f8a9efc71edcad6be19e2b5b7c21
winandtu <jefersson.arevalo@correounivalle.edu.co>
2020-03-29 03:10:01 +0000

    Ejercito.h y Ejercito.cpp comentados

--- ejercito.cpp
+++ ejercito.cpp
@@ -1,32 +1,34 @@
 #include "ejercito.h"
 
+//Aqui cada cantidad de cada tipo de avatares(cantidadLuchadores,cantidadTiradores, cantidadMagos) guardara los valores que se introduciran en el ejercito ejemplo: 15 luchadores, 12 tiradores, 10 magos.
 Ejercito::Ejercito(int numeroLuchadores, int numeroTiradores, int numeroMagos){
 
     cantidadLuchadores =numeroLuchadores; 
     cantidadTiradores=numeroTiradores;
     cantidadMagos=numeroMagos;
     setCantidadEjercito( cantidadLuchadores,  cantidadTiradores, cantidadMagos);
-  //Cual = Cual avatar.
+  
 
     
 } 
 
+//Esta implementacion no dara la cantidad total de los luchadores
 int Ejercito::getCantidadLuchadores(){
   return cantidadLuchadores;
 }
 
-
+//Esta implementacion no dara la cantidad total de los tiradores
 int Ejercito::getCantidadTiradores(){
   return cantidadTiradores;
 }
 
-
+//Esta implementacion no dara la cantidad total de los magos
 int Ejercito::getCantidadMagos(){
   return cantidadMagos;
 }
 
 
-//setea el ejercito modifica los valores.
+//Setea el ejercito con el fin de modificar sus valores
 
 void Ejercito::setCantidadEjercito(int luchadores, int tiradores, int magos){
     ejercitoAvatar.clear();
@@ -34,15 +36,15 @@ void Ejercito::setCantidadEjercito(int luchadores, int tiradores, int magos){
       ejercitoAvatar.push_back(new Avatar("luchador")); //Añado al vector la cantidad de luchadores que el usuario tiene 
     }
     for(int cual = 0; cual < tiradores ; cual++){
-      ejercitoAvatar.push_back(new Avatar("tirador"));
+      ejercitoAvatar.push_back(new Avatar("tirador")); //Añado al vector la cantidad de tiradores que el usuario tiene 
     }
     for(int cual = 0; cual < magos ; cual++){
-      ejercitoAvatar.push_back(new Avatar("mago"));
+      ejercitoAvatar.push_back(new Avatar("mago")); //Añado al vector la cantidad de magos que el usuario tiene 
     }
 
 }
 
-// obtengo los valores modificados o actuales
+// Esta implementacion obtendra los valores modificados o los valores que se actualizaran para dar el ejercito
 vector<Avatar*> Ejercito::getEjercito(){ //cree un nuevo tipo el cual es: vector<*avatar>
   return ejercitoAvatar;
 }
--- ejercito.h
+++ ejercito.h
@@ -1,16 +1,17 @@
 #ifndef EJERCITO_H
-#define EJERCITO_H
+#define EJERCITO_H //Guardas
 
 #include "avatar.h"
 #include <vector>
 using namespace std;
 
+//Se creo la clase ejercito la cual tendra atributos protegidos, los cuales nos serviran para guardar los valores que se introduzcan en el constructor, de igual forma el constructor tendra tres parametros que seran los grupos de cada tipo de avatares.
 class Ejercito{
   protected:
     int cantidadLuchadores, cantidadTiradores, cantidadMagos, cantidadEjercito; 
     vector<Avatar*> ejercitoAvatar; //avatar* es un vector de punteros a la clase avatar
   public: 
-  
+  //Seran los metodos que nos servira para dar la cantidad total de cada tipo de avatar y de igual forma nos dara la suma total de cada ejercito de avatares.
     Ejercito(int numeroLuchadores, int numeroTiradores, int numeroMago);
     void calcularEjercito(); 
     int getCantidadLuchadores();
